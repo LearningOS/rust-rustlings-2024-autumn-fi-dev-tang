8405295c670077ee3cfe5211bbb8ed5ca0cf5514
fi-dev-tang <3781768154@qq.com>
2024-11-20 17:28:15 +0800

    finish before generic and trait

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,31 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+/*
+[psNote]: 一个错误说明
+之前的写法就是
+impl AppendBar for Vec<String>{
+    fn append_bar(self) -> Self {
+        self.push(String::from("Bar"))
+    }
+}
+
+直接查看 push 的返回值，发现 push 返回类型是 (), 实际不返回值
+显式地返回被修改过的向量 `self`, 定义类型为 mut self
+因为 push 方法会改变向量的状态，最后返回 self 遵循方法的签名要求，确保返回的类型是 `Self`
+*/
+impl AppendBar for Vec<String>{
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
