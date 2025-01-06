ceb7b11e3dac90833be665a256689caf3381af31
fi-dev-tang <3781768154@qq.com>
2025-01-06 14:29:46 +0800

    Iterators for HashMap, usage: .iter().map().filter().count or flatmap(|map| map.values())

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,8 +11,6 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -32,10 +30,29 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
     count
 }
 
+/*
+这里希望使用 HashMap 的 iterator, 同时又强调了不能使用 for, while 等循环
+对类型进行特别说明:
+1. 首先传入的参数是 
+map: &HashMap<String, Progress> 
+2. 接着对 map 转换成 iter(), map.iter() 之后的类型是
+Iter<'_, String, Progress>
+具体迭代时产生的是 (&String, &Progress) 类型的元组
+3. 之后的 find(|key, value|: &(&String, &Progress))
+    所以元素的类型是 &&String, &&Progress 
+    解引用的时候得到的是 **v = Progress
+4. 使用 &v 相当于解引用一次, 相当于 &Progress 和 Progress 进行比较。
+5. 对于 PartialEq 的 trait 来说，rust会自动处理引用的解引用。
+解引用一次之后， &v 相当于 &Progress, 可以直接和 Progress 相比较。
+*/
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    let map_iter: usize = map
+                        .iter()    // 把 HashMap 转换成 iterator
+                        .filter(|(_, v)| **v == value)
+                        .count();
+    map_iter
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -50,11 +67,32 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
     count
 }
 
+/*
+这里的 &[HashMap<String, Progress>] 是 HashMap<String, Progress> 类型元素的数组。
+想要考察切片 slice 上的 map 方法类型转换
+
+现在给出的类型是: &[HashMap<String, Progress>]
+1. iter() 方法，转换成 
+Iterator<&HashMap<String, Progress>>
+2. map() 方法，转换每个 HashMap 为其值
+Iterator<Value<String, Progress>>
+3. flat_map() 方法，展平每个 Hashmap 的值
+Iterator<&Progress>
+
+4. flat_map(|map|map.values())
+接受一个参数 map, map 是对 HashMap<String, Progress> 的不可变引用。
+闭包返回的是 Values<'_, String, Progress> 类型的迭代器，迭代 Progress 值。
+*/
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    let collection_iter: usize = collection
+    .iter()
+    .flat_map(|map| map.values())
+    .filter(|v| **v == value)
+    .count();
+    collection_iter
 }
 
 #[cfg(test)]
