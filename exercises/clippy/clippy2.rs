0ef29eca90a35ddbec4f5d93f2706827c99ebc01
fi-dev-tang <3781768154@qq.com>
2025-02-07 17:20:38 +0800

    clippy helps write better code

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,12 +3,13 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    /*
+    clippy 提供更好的写法建议，对于设置成 Some(12) 类型的数据，最好对 Some 或者 None 做一个判断
+     */
+    if let Some(x) = option {
         res += x;
     }
     println!("{}", res);
--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,28 +4,37 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        my_option.unwrap();
+    if let Some(item) = my_option{
+        // 直接去掉 my_option.unwrap(), 因为 if let Some(item) = my_option 已经安全解包
+        // 这里的 Option 包裹的是一个单元类型 ()
     }
 
     let my_arr = &[
-        -1, -2, -3
+        -1, -2, -3,
         -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    /*
+    问题在于 my_arr.resize(0, 5)
+    第一个参数是修改数组的长度为 5, 直接返回一个单元类型() ,表示无信息量，所以这里的绑定没有意义，直接删掉 let binding
+     */
+    println!("This Vec is empty, see? {:?}", ());
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    /*
+    这里重新回顾一下所有权的问题
+    std::mem::swap(&mut value_a, &mut value_b);
+    接收的是两个变量的可变引用，执行之后 value_a 和 value_b 的值会互换，
+    但是它们的所有权仍然分别属于 value_a 和 value_b
+    不会改变变量的所有权，只是改变它们所指向的值。
+    在不转移所有权的情况下交换两个变量的值
+     */
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
